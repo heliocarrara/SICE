79aa8d52691d16795a4bec91acad0351a8c6e031
heliocarrara <heliocarrara@gmail.com>
2023-08-13 14:24:38 -0400

    Atualização de versão do bootstrape criação de area comum.

---
+++ Areas/Comum/Controllers/VisitaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SICE.Areas.Comum.Controllers
+{
+    public class VisitaController : Controller
+    {
+        // GET: Comum/Visita
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home", new { Area = ""});
+        }
+
+        public ActionResult GerenciarVisitas()
+        {
+            try
+            {
+                return View();
+            }
+            catch(Exception ex)
+            {
+                return Index();
+            }
+        }
+    }
+}
